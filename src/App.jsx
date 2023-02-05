a5589d2a1c812e6d6f9aa6fdb2b423dad278c9c3
Vincent Liussyaputra <liusvinv@gmail.com>
2023-02-05 22:40:27 +0700

    Add mock data

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,51 @@
 import { useState } from 'react'
 
 function App() {
-
+  const apiRes = {
+    "coord": {
+        "lon": 106.8451,
+        "lat": -6.2146
+    },
+    "weather": [{
+        "id": 804,
+        "main": "Clouds",
+        "description": "overcast clouds",
+        "icon": "04n"
+    }],
+    "base": "stations",
+    "main": {
+        "temp": 30.8,
+        "feels_like": 34.43,
+        "temp_min": 27.72,
+        "temp_max": 31.64,
+        "pressure": 1006,
+        "humidity": 60
+    },
+    "visibility": 10000,
+    "wind": {
+        "speed": 11.62,
+        "deg": 353,
+        "gust": 4.92
+    },
+    "clouds": {
+        "all": 100
+    },
+    "dt": 1675600021,
+    "sys": {
+        "type": 2,
+        "id": 2073276,
+        "country": "ID",
+        "sunrise": 1675551352,
+        "sunset": 1675595835
+    },
+    "timezone": 25200,
+    "id": 1642911,
+    "name": "Jakarta",
+    "cod": 200
+}
   return (
     <div className="App">
-    
+        <p>{JSON.stringify(apiRes)}</p>
     </div>
   )
 }
