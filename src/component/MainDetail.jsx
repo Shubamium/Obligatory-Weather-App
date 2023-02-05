02784a8de2b2c9a08ea8529bcb2a3a7eb2afd4a8
Vincent Liussyaputra <liusvinv@gmail.com>
2023-02-06 06:35:03 +0700

    Add more specific layouting

--- src/component/MainDetail.jsx
+++ src/component/MainDetail.jsx
@@ -17,19 +17,24 @@ export default function MainDetail() {
     return (
         <>
             <div className="weather-general">
-                    <h2>{wd.name}</h2>
-                    {countryFlag && <p>{countryFlag.name}</p>}
-
-                    <h2>{wd.weather[0].main}</h2>
-                    <p>{capitalize(wd.weather[0].description)}</p>
-                    <img src={`http://openweathermap.org/img/wn/${wd.weather[0].icon}@2x.png`} alt="weather-icon" className="weather-icon"/>
+                   <div className="weather-general_country">
+                        <div className="text">
+                            <h2>{wd.name}</h2>
+                            {countryFlag && <p>{countryFlag.name}</p>}
+                        </div>
+                        {countryFlag ? <img src={countryFlag.image} alt="country flag" width="100" className='country-flag' /> : <p>Loading Flags. . .</p>}
+                   </div>
                     <h2>{wd.main.temp}°</h2>
                     <p>Cloudiness:{wd.clouds.all}%</p>
             </div>
 
             <div className="country-data">
+                <img src={`http://openweathermap.org/img/wn/${wd.weather[0].icon}@2x.png`} alt="weather-icon" className="weather-icon"/>
+                <h2>{wd.weather[0].main}</h2>
+                <p>{capitalize(wd.weather[0].description)}</p>
+
                 {error}
-                {countryFlag ? <img src={countryFlag.image} alt="country flag" width="100" className='country-flag' /> : <p>Loading Flags. . .</p>}
+               
                 <p>{unixToLocalTime(wd.dt)}</p>
                 <p>{wd.coord.lat}°N {wd.coord.lon}°W</p>
             </div>
